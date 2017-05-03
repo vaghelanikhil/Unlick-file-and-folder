7448981cb5eecb827c41618ed7446d5ef2cebc10
vaghelanikhil <vaghelanikhil@users.noreply.github.com>
2017-05-03 10:08:56 +0530

    Update unlink.php

--- unlink.php
+++ unlink.php
@@ -1,11 +1,12 @@
 <php
-$temp_dir = "Your folder path like d:/xampp/htdocs/your project folder/temp"
+$temp_dir = "Your folder path like d:/xampp/htdocs/your project folder/temp";
 array_map('unlink', glob("$temp_dir/*.*")); // remove all files and folder in you temp directory
-rmdir($temp_dir); / remove your temp folder if empty 
+rmdir($temp_dir); // remove your temp folder if empty 
 
 /** array_map()
 
 ==> Applies the callback to the elements of the given arrays
+==> Send each value of an array to a function, multiply each value by itself, and return an array with the new values
 ==> http://php.net/manual/en/function.array-map.php
 **/
 ?>
